f72a9a386da3bea147c2754449c4c549de1f8619
Yash <yashbjaj@outlook.com>
2022-01-21 19:12:20 +0530

    ready to deply

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+// use the express-static middleware
+app.use(express.static("static"))
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send(index.html)
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
